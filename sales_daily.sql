efab196dadd21ab481187a387ddc9f27aa33e6c8
eeliskab <149785118+eeliskab@users.noreply.github.com>
2024-01-08 16:48:57 +0100

    Update sales_daily.sql
    changing daily purchase cost to daily discount

--- sales_daily.sql
+++ sales_daily.sql
@@ -1,7 +1,7 @@
 SELECT 
 date_date
 , ROUND(SUM(turnover),2) AS daily_turnover
-, ROUND(SUM(purchase_cost),2) AS daily_purchase_cost
+, ROUND(SUM((turnover_before_promo - turnover),2) AS daily_discount
 FROM data-analytics-bootcamp-363212.course14.gwz_sales
 GROUP BY date_date
 ORDER BY date_date DESC
