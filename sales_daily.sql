bb7503500cf4ea1c01773c11417839cbdcfc3bef
eeliskab <149785118+eeliskab@users.noreply.github.com>
2024-01-10 16:37:31 +0100

    Update sales_daily.sql
    idk

--- sales_daily.sql
+++ sales_daily.sql
@@ -1,8 +1,9 @@
 SELECT 
-date_date
+category_1
+, date_date
 , ROUND(SUM(turnover),2) AS daily_turnover
-, ROUND(SUM((turnover_before_promo - turnover),2) AS daily_discount
+, ROUND(SUM(turnover_before_promo ),2) AS daily_turnover_before_promo
 , ROUND(SUM(turnover) - SUM(purchase_cost), 2) AS margin
 FROM data-analytics-bootcamp-363212.course14.gwz_sales
-GROUP BY date_date
-ORDER BY date_date DESC
+GROUP BY category_1, date_date
+ORDER BY category_1 DESC, date_date 
